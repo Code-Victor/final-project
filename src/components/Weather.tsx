f466e8172a5cd6de40ff89cb0adda91d22cb6ea5
Hamzat Victor <oluwaborihamzat@gmail.com>
2023-12-19 22:17:22 +0100

    fix: error

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -9,7 +9,7 @@ const Weather = () => {
     <div className="bg-gray-300 text-gray-900 px-6 py-2 rounded-md fixed bottom-5 left-5">
       <div className="flex gap-2">
         <img src={iconurl} alt="weather icon" />
-        <h2 className="font-bold text-lg">{weatherData.description}</h2>
+        <h2 className="font-bold text-lg">{weatherData?.description}</h2>
       </div>
       <h3 className="font-bold">{data?.name}</h3>
     </div>
